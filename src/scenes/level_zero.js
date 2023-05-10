96e02d776dd34831c2a7f844b09ac6c425ff184e
Matheus Loiola <matheusloiolapinto@gmail.com>
2023-05-09 23:55:55 -0300

    feat: improved fog of war

--- src/scenes/level_zero.js
+++ src/scenes/level_zero.js
@@ -8,13 +8,13 @@ export default class LevelZero extends Phaser.Scene {
     preload() {
         this.load.image('tiles', 'assets/tilemaps/level0_template.png');
         this.load.tilemapTiledJSON('map', 'assets/tilemaps_json/level0_template.json');
-        this.load.spritesheet('player', 'assets/sprites/player.png', {frameWidth: 16, frameHeight: 16});
-        this.load.spritesheet('devour', 'assets/sprites/devour.png', {frameWidth: 32, frameHeight: 32});
-        this.load.spritesheet('lantern', 'assets/sprites/lantern.png', {frameWidth: 32, frameHeight: 32});
-        this.load.spritesheet('walker', 'assets/sprites/walker.png', {frameWidth: 16, frameHeight: 16});
-        this.load.spritesheet('partygoer', 'assets/sprites/partygoer.png', {frameWidth: 32, frameHeight: 32});
-        this.load.spritesheet('meatball', 'assets/sprites/meatball.png', {frameWidth: 16, frameHeight: 16})
-        this.load.spritesheet('smile', 'assets/sprites/smile.png', {frameWidth: 16, frameHeight: 16})
+        this.load.spritesheet('player', 'assets/sprites/player.png', { frameWidth: 16, frameHeight: 16 });
+        this.load.spritesheet('devour', 'assets/sprites/devour.png', { frameWidth: 32, frameHeight: 32 });
+        this.load.spritesheet('lantern', 'assets/sprites/lantern.png', { frameWidth: 32, frameHeight: 32 });
+        this.load.spritesheet('walker', 'assets/sprites/walker.png', { frameWidth: 16, frameHeight: 16 });
+        this.load.spritesheet('partygoer', 'assets/sprites/partygoer.png', { frameWidth: 32, frameHeight: 32 });
+        this.load.spritesheet('meatball', 'assets/sprites/meatball.png', { frameWidth: 16, frameHeight: 16 })
+        this.load.spritesheet('smile', 'assets/sprites/smile.png', { frameWidth: 16, frameHeight: 16 })
         this.load.image('vision', 'assets/mask.png')
     }
 
@@ -52,14 +52,9 @@ export default class LevelZero extends Phaser.Scene {
             height
         }, true)
 
+
         // fill it with black
-        this.rt.fill(0x000000, 0.95)
-
-        // draw the floorLayer into it
-        this.rt.draw(belowLayer)
-
-        // set a dark blue tint
-        this.rt.setTint(0x0a2948);
+        this.rt.fill(0x000000, 0.9)
         this.rt.setDepth(100)
 
         this.vision = this.make.image({
@@ -68,9 +63,9 @@ export default class LevelZero extends Phaser.Scene {
             key: 'vision',
             add: false
         })
-        this.vision.scale = 0.3
+        this.vision.scale = 0.4
         this.vision.setPosition(this.player.x, this.player.y)
-    
+
         this.rt.mask = new Phaser.Display.Masks.BitmapMask(this, this.vision)
         this.rt.mask.invertAlpha = true
         // this.rt.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
