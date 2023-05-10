b79f9327e479106a6c3994ad63ef08b1e2342dd8
loioladev <matheusloiolapinto@gmail.com>
2023-05-10 09:50:30 -0300

    Finished level zero

--- src/scenes/level_zero.js
+++ src/scenes/level_zero.js
@@ -22,18 +22,18 @@ export default class LevelZero extends Phaser.Scene {
         const map = this.make.tilemap({ key: 'map' });
         const tileset = map.addTilesetImage('level0_template', 'tiles');
 
+        // Create layers and set collision
         const belowLayer = map.createLayer('Below', tileset, 0, 0);
         const worldLayer = map.createLayer('World', tileset, 0, 0);
         const decorationLayer = map.createLayer('Decoration', tileset, 0, 0);
         const aboveLayer = map.createLayer('Above', tileset, 0, 0);
         const above1Layer = map.createLayer('Above1', tileset, 0, 0);
-
         worldLayer.setCollisionByProperty({ collider: true });
         decorationLayer.setCollisionByProperty({ collider: true });
         aboveLayer.setDepth(10);
         above1Layer.setDepth(10);
 
-        // Create the player with physics
+        // Create sprites for map
         const spawnPoint = map.findObject("Object", obj => obj.name === "Spawnpoint");
         this.player = new Player(this, spawnPoint.x, spawnPoint.y, 'player');
         this.monster1 = new Monster(this, spawnPoint.x, spawnPoint.y, 'lantern', 'lantern');
@@ -43,47 +43,44 @@ export default class LevelZero extends Phaser.Scene {
         this.monster5 = new Monster(this, spawnPoint.x - 32, spawnPoint.y, 'meatball', 'meatball')
         this.monster6 = new Monster(this, spawnPoint.x, spawnPoint.y + 64, 'smile', 'smile')
 
+        // Make fog of war effect
         const width = this.scale.width * 2
         const height = this.scale.height * 2
-
-        // make a RenderTexture that is the size of the screen
         this.rt = this.make.renderTexture({
             width,
             height
         }, true)
-
-
-        // fill it with black
         this.rt.fill(0x000000, 0.9)
         this.rt.setDepth(100)
-
         this.vision = this.make.image({
             x: this.player.x,
             y: this.player.y,
             key: 'vision',
             add: false
         })
-        this.vision.scale = 0.4
+        this.vision.scale = 0.3
         this.vision.setPosition(this.player.x, this.player.y)
-
         this.rt.mask = new Phaser.Display.Masks.BitmapMask(this, this.vision)
         this.rt.mask.invertAlpha = true
-        // this.rt.setBounds(0, 0, map.widthInPixels, map.heightInPixels)
 
+        // Add player physics
         this.physics.add.collider(this.player, worldLayer);
         this.physics.add.collider(this.player, decorationLayer);
 
-        // make camera follow player
+        // Make camera follow player
         this.cameras.main.startFollow(this.player);
         this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
 
+        // Add player movement
         this.cursors = this.input.keyboard.createCursorKeys();
     }
 
     update() {
+        // Update player movement
         const speed = 80; // pixels per second
         this.player.update(this.cursors, speed);
 
+        // Update fog of war effect
         this.rt.x = this.player.x;
         this.rt.y = this.player.y;
         this.vision.x = this.player.x;
